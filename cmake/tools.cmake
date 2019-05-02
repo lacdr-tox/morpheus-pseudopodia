6d0e59fe2abe93580c2537b6c2f3b19f12712c95
Jörn Starruß <joern.starruss@tu-dresden.de>
2019-05-02 14:52:37 +0200

    Build system rework Part II

---
+++ cmake/tools.cmake
@@ -0,0 +1,22 @@
+FUNCTION(list_prepend IN prefix OUT)
+	SET(_list "")
+	FOREACH(entry ${${IN}})
+		LIST(APPEND _list "${prefix}${entry}")
+	ENDFOREACH()
+	SET(${OUT} ${_list} PARENT_SCOPE)
+endfunction()
+
+FUNCTION(target_sources_relpaths target iface sources)
+	IF (CMAKE_VERSION VERSION_LESS 3.13)
+		get_target_property( target_path ${target} SOURCE_DIR)
+		FOREACH(entry ${${sources}})
+			message(STATUS "Adding entry ${CMAKE_CURRENT_SOURCE_DIR}/${entry} to target ${target}")
+# 			FILE(RELATIVE_PATH rpath ${target_path} ${CMAKE_CURRENT_SOURCE_DIR}/${entry})
+			target_sources(${target} ${iface} ${CMAKE_CURRENT_SOURCE_DIR}/${entry})
+		ENDFOREACH()
+	ELSE()
+		target_sources(${target} ${iface} ${sources})
+	ENDIF()
+
+
+endfunction()
