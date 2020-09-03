c058631b5373d265264ed79dafb9c6b6ecba9e1d
Jörn Starruß <joern.starruss@tu-dresden.de>
2020-09-03 17:52:24 +0200

    fix typo

--- gui/morpheusML/morpheus_model.cpp
+++ gui/morpheusML/morpheus_model.cpp
@@ -1146,7 +1146,7 @@ QModelIndex MorphModel::insertNode(const QModelIndex &parent, QString child, int
 	
 		if ( ! contr )
 			throw ModelException(ModelException::InvalidNodeIndex,QString("MorphModel::insertNode: Request to insert into invalid index!"));
-		if (!contr->getAddableChilds().contains(child))
+		if (!contr->getAddableChilds(true).contains(child))
 			throw ModelException(ModelException::UndefinedNode,QString("MorphModel::insertNode: Requested node %1 cannot be inserted!").arg(child));
 		prepareActivationOrInsert(contr, child);
 		
